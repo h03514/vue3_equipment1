71f2f6a35436ff02351cf371ccb243736003f4f5
Mark <h03514@gmail.com>
2021-08-30 23:44:42 +0800

    製作畫面

--- src/components/testEmit2.vue
+++ src/components/testEmit2.vue
@@ -1,10 +1,6 @@
 <template>
   <div v-if="msg == 'Computer'">
-    <div
-      class="row Computer"
-      v-for="(item, key) in equipment.computer"
-      :key="key"
-    >
+    <div class="row Computer" v-for="(item, key) in equipment.computer" :key="key">
       <div class="col-sm-3">{{ item.qvesn }}</div>
       <div class="col-sm-2">{{ item.title }}</div>
       <div class="col-sm-3">{{ item.type }}</div>
@@ -16,11 +12,7 @@
     </div>
   </div>
   <div v-if="msg == 'Monitor'">
-    <div
-      class="row Monitor"
-      v-for="(item, key) in equipment.monitor"
-      :key="key"
-    >
+    <div class="row Monitor" v-for="(item, key) in equipment.monitor" :key="key">
       <div class="col-sm-3">{{ item.qvesn }}</div>
       <div class="col-sm-2">{{ item.title }}</div>
       <div class="col-sm-3">{{ item.type }}</div>
@@ -31,27 +23,39 @@
       </div>
     </div>
   </div>
+  <div>
+    <hr v-if="abc">
+    <div class="row" v-for="(item , index) in testOBj" :key="index">
+      <div class="col-4">{{item.gender}}</div>
+      <div class="col-4">{{item.name.first}}</div>
+      <div class="col-4">{{item.name.last}}</div>
+    </div>
+  </div>
 </template>
 
 <script>
 import emitter from "../assets/js/mitt";
-import axios from 'axios';
+import axios from "axios";
 import { ref, reactive, onMounted } from "vue";
-
 
 export default {
   name: "testEmit2",
-  methods:{
-    async ddusios(){
-      await axios.get('https://randomuser.me/api/')
-           .then((res)=>{
-               console.log(res);
-           })
+  data() {
+    return {
+      testOBj: {},
+      abc: false
+    };
+  },
+  methods: {
+    async ddusios() {
+      await axios.get("https://randomuser.me/api/?results=5").then(res => {
+        this.testOBj = res.data.results;
+        this.abc = true;
+      });
     }
   },
 
   setup() {
-
     let msg = ref("");
     let wer = ref("");
 
@@ -62,22 +66,22 @@ export default {
           qvesn: "216911001",
           title: "supperlier-01",
           type: "computer",
-          model: "520MT",
+          model: "520MT"
         },
         {
           id: 2,
           qvesn: "216911002",
           title: "supperlier-02",
           type: "computer",
-          model: "520MT",
+          model: "520MT"
         },
         {
           id: 3,
           qvesn: "216911003",
           title: "supperlier-03",
           type: "computer",
-          model: "520MT",
-        },
+          model: "520MT"
+        }
       ],
       monitor: [
         {
@@ -85,45 +89,42 @@ export default {
           qvesn: "216911004",
           title: "supperlier-04",
           type: "Monitor",
-          model: "ViewSonic",
+          model: "ViewSonic"
         },
         {
           id: 2,
           qvesn: "216911005",
           title: "supperlier-05",
           type: "Monitor",
-          model: "Benq",
+          model: "Benq"
         },
         {
           id: 3,
           qvesn: "216911006",
           title: "supperlier-06",
           type: "Monitor",
-          model: "AOC",
-        },
-      ],
+          model: "AOC"
+        }
+      ]
     });
 
-    emitter.on("hello", (val) => {
+    emitter.on("hello", val => {
       msg.value = val.value;
     });
 
-    emitter.on("send", (val) => {
+    emitter.on("send", val => {
       msg.value = val.value;
     });
-    emitter.on("ddd", (val) => {
+    emitter.on("ddd", val => {
       wer.value = val.value;
     });
 
     const state = reactive({
-      hits: [],
+      hits: []
     });
 
-
-
-
     // function ddusios() {
-      
+
     //   VueAxios({
     //     method: "get",
     //     url: "https://randomuser.me/api/"
@@ -147,8 +148,8 @@ export default {
       msg: msg,
       wer: wer,
       equipment,
-      state,
+      state
     };
-  },
+  }
 };
 </script>
